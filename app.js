65a986312cefbf0dceaf2fa8289f36d29c2d1000
AndrewNguyen192257 <andrewnguyenwebdev192257@gmail.com>
2024-08-21 17:48:31 +0700

    Dinh nghia cac route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouters = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouters);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
